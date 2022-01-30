84ad2a0d477ad40c1431bb3440a8b37907575106
cogrady42 <ceciliaogrady42@gmail.com>
2022-01-30 17:15:17 -0500

    added forecast html/css

--- src/index.js
+++ src/index.js
@@ -55,12 +55,6 @@ function displayWeather(response) {
   document.querySelector("#current-time").innerHTML = formatDate(
     response.data.dt * 1000
   );
-  let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function displayFahrenheitTemperature(event) {
